645a62cd222a209fbbe1e8110c20d2566af4dd17
Adem Özdemir <ozadem.98@gmail.com>
2022-05-28 00:34:58 +0300

    28/05/2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,10 +9,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--------------------------ProductTest-----------------------------------------");
+
+            ProductTest();
+            Console.WriteLine("--------------------------CategoryTest-----------------------------------------");
+            CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(50,100) )
-            {  
-                Console.WriteLine(product.UnitPrice);
+            foreach (var product in productManager.GetAllByCategoryId(1))
+            {
+                Console.WriteLine(product.ProductName);
             }
         }
     }
