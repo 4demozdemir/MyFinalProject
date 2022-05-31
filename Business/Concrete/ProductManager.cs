c1255ac73dcf018658c6b8aabbc152c11fcd4667
Adem Özdemir <ozadem.98@gmail.com>
2022-05-31 17:28:06 +0300

    added is web api

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Absract;
+using Core.Utilities.Results;
 using DataAccess.Abstarct;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -17,6 +18,19 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
+
+        public IResult Add(Product product)
+        {
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult("ürün ismi en az 2  karakter olmalıdır");
+            }
+            _productDal.Add(product);
+
+            return new SuccessResult();
+
+        }
+
         public List<Product> GetAll()
         {   // İş kodları
             return _productDal.GetAll();
@@ -25,6 +39,12 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p=> p.CategoryId==id);
+        }
+
+        public Product GetById(int productId)
+        {
+
+            return _productDal.Get(p=> p.ProductId==productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
